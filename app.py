c8a5e7a789f742d6959ab08ae5149f8c12c0a851
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-05-29 17:04:21 +0000

    Add .env to .gitignore and implement exchange rate fetching in app.py

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ import requests
 
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
+url=os.getenv("BASE_URL")
+api_key=os.getenv("EXCHANGE_API_KEY")
+headers = {"accept": "application/json"}
+
+exchange_res=requests.get(f'{url}?app_id={api_key}',headers=headers)
+
+print(exchange_res.json())
+
+exchange_rates=exchange_res.json()["rates"]
+
+for currency, rate in exchange_rates.items():
+    print(f"{currency}: {rate}")
